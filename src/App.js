2453360641b8c954a7641d73e72e6dce44e365af
Murilo Amaral <muriloamaciel@gmail.com>
2022-04-14 09:29:34 -0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+const oneSecond = 1000; // one second same as 1000ms
+const counterTimer = 25 * 60; //timedown minutes
 
 function App() {
+  const [Counter, setCounter] = useState(counterTimer);
+
+  useEffect(() => {
+      setTimeout(() => {
+        setCounter((state) => state - 1);
+      }, oneSecond);
+    }, [Counter]);
+  
+  const mm = Math.floor(Counter / 60);
+  const ss = Math.floor(Counter % 60);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="timer">
+        <h1> Cronometro</h1>
+
+        <span>{String(mm).padStart(2, "0")}</span>
+        <span>:</span>
+        <span>{String(ss).padStart(2, "0")}</span>
+      </div>
+      <div className="Menu-button">
+        <button type="button" >
+          Start
+        </button>
+        <button type="button" >
+          Pause
+        </button>
+        <button type="button" >
+          Stop
+        </button>
+      </div>
+    </>
   );
 }
 
